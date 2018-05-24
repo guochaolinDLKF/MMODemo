d5dd042af743ade8cdb27b1d4ccdbb84d2f581e2
guochaolinDLKF <guochaolinvr@163.com>
2018-05-24 16:30:02 +0800

    1.0.6Desion

---
+++ Desion/SQL/mmodb.sql
@@ -0,0 +1,60 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80011
+ Source Host           : localhost:3306
+ Source Schema         : mmodb
+
+ Target Server Type    : MySQL
+ Target Server Version : 80011
+ File Encoding         : 65001
+
+ Date: 24/05/2018 16:29:29
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for accountinfo
+-- ----------------------------
+DROP TABLE IF EXISTS `accountinfo`;
+CREATE TABLE `accountinfo`  (
+  `accountid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0',
+  `accountname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `imgpath` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`accountid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for characterinfo
+-- ----------------------------
+DROP TABLE IF EXISTS `characterinfo`;
+CREATE TABLE `characterinfo`  (
+  `id` int(11) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+  `accountid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `level` int(11) NULL DEFAULT NULL COMMENT '等级',
+  `curexp` int(11) NULL DEFAULT NULL COMMENT '经验',
+  `curstrength` int(11) NULL DEFAULT NULL COMMENT '体力',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `accountid`(`accountid`) USING BTREE,
+  CONSTRAINT `characterinfo_ibfk_1` FOREIGN KEY (`accountid`) REFERENCES `accountinfo` (`accountid`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for inventoryinfo
+-- ----------------------------
+DROP TABLE IF EXISTS `inventoryinfo`;
+CREATE TABLE `inventoryinfo`  (
+  `id` int(11) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+  `inventoryid` int(11) NULL DEFAULT NULL COMMENT 'id',
+  `buyprice` int(11) NULL DEFAULT NULL,
+  `shellprice` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
